bb8a22dc11a9328d5eb0c4ff75ffb39b1f6bf2c5
Lazbert <lambert.leung.1@gmail.com>
2025-09-30 03:54:25 +0800

    enable svgr and path alias in tsconfig.json

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+// svg.d.ts
+declare module '*.svg' {
+    import React = require('react');
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
