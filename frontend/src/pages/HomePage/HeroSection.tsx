436a82888715e819d36f13fbc98e867a3b33eb72
Lazbert <lambert.leung.1@gmail.com>
2025-09-30 19:32:51 +0800

    feat(ui): HeroSection and InteractiveEarth

--- frontend/src/pages/HomePage/HeroSection.tsx
+++ frontend/src/pages/HomePage/HeroSection.tsx
@@ -1,5 +1,22 @@
-const HeroSection: React.FC = () => {
-  return <div>HeroSection</div>;
+import IconGMapsPin from '../../assets/IconGMapsPin.svg?react';
+
+const HeroSection = () => {
+  return (
+    <div className="relative flex flex-col items-center gap-6 px-5">
+        <span className="font-semibold text-[64px] text-dark-lime leading-tight text-left px-2.5">
+            Skill issues?
+            <br />
+            <span className='text-smoke text-[36px]'>
+                Start guessing like you've been living there
+            </span>
+        </span>
+        <span className="text-2xl text-grey leading-tight text-left px-2.5">
+            The all-in-one platform to compile all metas into a compendium<br />
+            for Geoguessr enthusiasts
+        </span>
+        <IconGMapsPin className="absolute top-[-2px] left-[368px] w-[195px] h-[195px]" />
+    </div>
+  );
 };
 
 export default HeroSection;
--- frontend/src/pages/HomePage/InteractiveEarth.tsx
+++ frontend/src/pages/HomePage/InteractiveEarth.tsx
@@ -1,5 +1,11 @@
-const InteractiveEarth: React.FC = () => {
-  return <div>InteractiveEarth</div>;
+import Earth from '../../assets/Earth2D.svg?react';
+
+const InteractiveEarth = () => {
+  return (
+    <div className="flex justify-center items-center gap-2.5 w-fit h-full">
+      <Earth className="w-[612px] h-[632px]" />
+    </div>
+  );
 };
 
 export default InteractiveEarth;
