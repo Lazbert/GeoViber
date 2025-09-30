fdd12aba23cdff9eea34a5be8f57bb4ae689a0f5
Lazbert <lambert.leung.1@gmail.com>
2025-09-30 23:44:06 +0800

    feat: Earth with heart beats

--- frontend/src/pages/HomePage/InteractiveEarth.tsx
+++ frontend/src/pages/HomePage/InteractiveEarth.tsx
@@ -3,7 +3,7 @@ import Earth from '../../assets/Earth2D.svg?react';
 const InteractiveEarth: React.FC = () => {
   return (
     <div className="flex justify-center items-center gap-2.5 w-fit h-full">
-      <Earth className="w-[612px] h-[632px]" />
+      <Earth className="w-[612px] h-[632px] animate-heartbeat" />
     </div>
   );
 };
